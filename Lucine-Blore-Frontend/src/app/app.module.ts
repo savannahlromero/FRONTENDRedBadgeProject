ef05d2ca6e1586ad1c03bea8292761f3f40df090
savannahlromero <savannahlromero@gmail.com>
2019-03-15 10:13:59 -0400

    added Modules folder + did some more register page work

--- Lucine-Blore-Frontend/src/app/app.module.ts
+++ Lucine-Blore-Frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -45,7 +46,8 @@ import { AuthService } from './services/auth.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [
     AuthService
---
+++ Lucine-Blore-Frontend/src/app/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+export const environment = {
+    production: true
+  };
+  
+  export let APIURL = '';
+  
+  switch (window.location.hostname) {
+      // this is the deployed angular application
+      case 'lucine-blore-frontend.herokuapp.com':
+          // this is the full url of your deployed API
+          APIURL = 'https://lucineandbloreluxuryvenues.azurewebsites.net/'
+          break;
+      default:
+          // this is the local host name of your API
+          APIURL = 'http://localhost:50976';
+  }
+  
