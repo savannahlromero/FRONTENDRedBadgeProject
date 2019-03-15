81b2ffa31e270be061f7fc86500eeab6bca1315f
savannahlromero <savannahlromero@gmail.com>
2019-03-15 09:57:50 -0400

    Refactored the folders

---
+++ Lucine-Blore-Frontend/src/app/services/auth.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { APIURL } from '../environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+}
