86792fdb76d4ec0b619de67dcf8b5f1f24a599f3
savannahlromero <savannahlromero@gmail.com>
2019-03-21 14:30:59 -0400

    review index done

--- Lucine-Blore-Frontend/src/app/components/review-home-listall/review-home-listall.component.ts
+++ Lucine-Blore-Frontend/src/app/components/review-home-listall/review-home-listall.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ReviewService} from '../../services/reviewservice';
+import { ReviewCreate } from '../../models/reviewcreatemodel';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-review-home-listall',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./review-home-listall.component.css']
 })
 export class ReviewHomeListallComponent implements OnInit {
-
-  constructor() { }
+  constructor(private _reviewService: ReviewService) { }
+  columnNames = ['details','ReviewID','VenueID', 'VenueRating', 'Comments', 'buttons'];
+  dataSource: MatTableDataSource<ReviewCreate>
 
   ngOnInit() {
+    this._reviewService.getReviews().subscribe((reviews: ReviewCreate[]) => {
+      this.dataSource = new MatTableDataSource<ReviewCreate>(reviews);
+    });
   }
 
 }
